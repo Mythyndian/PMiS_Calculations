c542d63a5bc0eef938f54342dbe68b0e041a95ae
Mythyndian <zawistowskimaciej1@gmail.com>
2022-02-01 13:15:48 +0100

    Initial commit

--- phase_space.py
+++ phase_space.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-t = np.linspace(0, 20, 10000)
-x = [2, 0]
+t = np.linspace(0, 1, 100)
+x = [1,1]
 delta = 0.62
 k = 30.5
 m = 59
@@ -16,9 +16,9 @@ def sho(t, x):
     return result
 
 
-solution = solve_ivp(sho, [0, 10000], y0=x, t_eval=t)
+solution = solve_ivp(sho, [0,100], y0=x, t_eval=t)
 plt.plot(solution.y[0], solution.y[1], color='b')
-# plt.axis([0, 6, -1.4, 0])
+# plt.axis([-4, 4,-0.6, 1])
 plt.title('Przestrzeń fazowa', fontsize=20)
 plt.xlabel("x")
 plt.ylabel("v")
